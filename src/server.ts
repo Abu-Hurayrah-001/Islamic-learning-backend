a0411c11d1c3537d30509557aeba5f78a8c9d194
dedic <dedicatedgamer59@gmail.com>
2024-10-15 20:02:04 +0530

    Added C.O.R.S. & reorganised files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, {Request, Response} from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT: number = Number(process.env.PORT) || 8000;
+
+//C.O.R.S. configuration
+app.use(cors({
+    origin: process.env.Frontend_Domain,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 //Middlewares
 app.use(express.json());
@@ -16,5 +23,5 @@ app.get("/", (req: Request, res: Response) => {
 
 //Starting the server
 app.listen(PORT, () => {
-    console.log(`Server is up and running on PORT: ${PORT}`);
-})
+    console.log(`YAY!!Server is up and running on PORT: ${PORT}`);
+});
