678611dd28f55afa395ac09457eb57c4dba3e8f5
dedic <dedicatedgamer59@gmail.com>
2024-10-17 20:52:02 +0530

    Refactored stuff

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,32 @@
-// Imports
+// IMPORTS
 import express, {Request, Response} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
+import { rateLimiter } from "./utils/rateLimiter";
 import helmet from "helmet";
+import homeRouter from "./routes/home.route";
 
-// loading .env
+// lOADING THE .ENV FILE
 dotenv.config();
 
+// CONSTS. AND VARS.
 const app = express();
 const PORT: number = Number(process.env.PORT) || 8000;
 
-// C.O.R.S. configuration
+// MIDDLEWARES
 app.use(cors({
     origin: process.env.Frontend_Domain,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
-// Rate limiting configuration
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-    message: "Too many requests from this I.P. (Kindly come back after 15 minutes)",
-    legacyHeaders: false,
-});
-
-//Middlewares
 app.use(helmet());
 app.use(express.json());
-app.use(limiter);
+app.use(rateLimiter);
 
-// Routes
-app.get("/", (req: Request, res: Response) => {
-    res.send("You are not supposed to be here, GOOD SIR!!");
-});
+// ROUTES
+app.use("/", homeRouter);
 
-// Starting the server
+// STARTING THE SERVER
 app.listen(PORT, () => {
     console.log(`YAY!!Server is up and running on PORT: ${PORT}`);
 });
