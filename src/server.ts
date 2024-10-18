cad24fe5ab0bdb498048c5a2b7fa7812fc555de0
dedic <sahilakhtar5059@gmail.com>
2024-10-18 08:44:35 +0530

    Added types to C.O.R.S

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // IMPORTS
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import { rateLimiter } from "./utils/rateLimiter";
 import helmet from "helmet";
 import homeRouter from "./routes/home.route";
@@ -13,15 +13,17 @@ dotenv.config();
 const app = express();
 const PORT: number = Number(process.env.PORT) || 8000;
 
-// MIDDLEWARES
-app.use(cors({
+const corsConfiguration: CorsOptions = {
     origin: process.env.Frontend_Domain,
     methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+}
 
+// MIDDLEWARES
+app.use(cors(corsConfiguration));
 app.use(helmet());
 app.use(express.json());
 app.use(rateLimiter);
+//TODO: Implement CSRF protection if I implemented cookies in auth
 
 // ROUTES
 app.use("/", homeRouter);
